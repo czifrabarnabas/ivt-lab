4bf90bbc6f628d700c55d739ace2bfd8202290b0
czifrabarnabas <barna.czifra@gmail.com>
2024-05-17 13:10:37 +0200

    throw exception

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -10,9 +10,13 @@ public class GT4500 implements SpaceShip {
 
   private boolean wasPrimaryFiredLast = false;
 
-  public GT4500() {
-    this.primaryTorpedoStore = new TorpedoStore(10);
-    this.secondaryTorpedoStore = new TorpedoStore(10);
+  public GT4500(TorpedoStore primary, TorpedoStore secondary) {
+    if(primary == null || secondary == null) {
+      this.primaryTorpedoStore = primary;
+      this.secondaryTorpedoStore = secondary;
+    }else {
+      throw new IllegalArgumentException();
+    }
   }
 
   public boolean fireLaser(FiringMode firingMode) {
